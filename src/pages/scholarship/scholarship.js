dc10ffd276ead92abd9312342e2e9bce9a2658e8
xinya.hao <xinya.hao@qingclass.com>
2020-05-07 13:50:26 +0800

    新增修改

---
+++ src/pages/scholarship/scholarship.js
@@ -0,0 +1,18 @@
+// pages/scholarship/scholarship.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {},
+
+  handleContact (e) {
+    console.log(e.detail.path)
+    console.log(e.detail.query)
+  }
+})
